8c990c83d899808985e6a5d1efab6f74bcfec5d5
Minse5k <pms16011172@gmail.com>
2021-06-13 14:26:48 +0900

    add day23 code

---
+++ programmers/가장 먼 노드/minseok_day23.js
@@ -0,0 +1,50 @@
+function createGraph(n, edge) {
+    return edge.reduce(
+        (graph, value) => {
+        const [start, end] = [value[0], value[1]];
+        
+        graph[start].push(end);
+        graph[end].push(start);
+        return graph;
+    }, new Array(n + 1).fill(null).map((v) => []));
+}
+
+function findMaxEdgeCount(distance) {
+    const maxNum = Math.max(...distance);
+    return distance.reduce(
+        (count, value) => {
+        if(value === maxNum) count++;
+        
+            return count;
+    }, 0);
+}
+
+function bfs(start, nodeArray, distance) {
+    const queue = [];
+
+    queue.push(start);
+    distance[start] = 1;
+    
+    while(queue.length > 0) {
+        const pre = queue.shift();
+        
+        for(const next of nodeArray[pre]) {
+            if(distance[next] !== 0) continue;
+            
+            distance[next] = distance[pre] + 1;
+            queue.push(next);
+        }
+    }
+    
+    return distance;
+}
+
+function solution(n, edge) {
+    
+    const nodeArray = createGraph(n, edge);
+    const distance = new Array(n + 1).fill(0);
+    
+    const distanceArray = bfs(1, nodeArray, distance);   
+    
+    return findMaxEdgeCount(distanceArray);
+}
