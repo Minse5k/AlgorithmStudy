373396a7c5d71dd579eaf449301724e4fb8dc19c
Sunjae95 <tjswo9502@gmail.com>
2021-06-24 00:35:20 +0900

    선재_행렬 테두리 회전하기

--- programmers/행렬 테두리 회전하기/seonjae_day24.js
+++ programmers/행렬 테두리 회전하기/seonjae_day24.js
@@ -1,58 +1,54 @@
 function solution(rows, columns, queries) {
-    var answer = [];
-    const board = getBoard(rows, columns);
-  
-    queries.reduce((preBoard, query) => {
-      return rotate(preBoard, query);
-    }, board);
-    return answer;
+  const board = getBoard(rows, columns);
+
+  const answer = queries.reduce((ans, query) => {
+    const min = rotate(board, query);
+    ans.push(min);
+
+    return ans;
+  }, []);
+
+  return answer;
+}
+
+function getBoard(rows, columns) {
+  const board = Array.from(Array(rows), () => new Array(columns).fill(0));
+  let num = 1;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = num++;
+    }
   }
-  
-  function getBoard(rows, columns) {
-    const board = new Array(rows)
-      .fill(1)
-      .map((value, index) => value * (index + 1));
-  
-    return board.reduce((pre, row) => {
-      const add = new Array(columns)
-        .fill(columns - 1)
-        .map((value, index) => row + value * (row - 1) + index);
-      return [...pre, add];
-    }, []);
+  return board;
+}
+
+function rotate(board, query) {
+  const [startRow, startColumn] = [query[0] - 1, query[1] - 1];
+  const [endRow, endColumn] = [query[2] - 1, query[3] - 1];
+
+  const tmp = board[startRow][startColumn];
+  let min = tmp;
+
+  for (let i = startRow; i < endRow; i++) {
+    board[i][startColumn] = board[i + 1][startColumn];
+    min = Math.min(min, board[i][startColumn]);
   }
-  
-  function rotate(board, query) {
-    const [startRow, startColumn, endRow, endColumn] = query;
-    //->
-    const LEFT = board[startRow - 1][endColumn - 1];
-    if (startColumn + 1 && endColumn) {
-      for (let i = endColumn - 1; i > startColumn - 1; i--) {
-        board[startRow - 1][i] = board[startRow - 1][i - 1];
-      }
-    }
-    //_
-    const DOWN = board[endRow - 2][endColumn - 1];
-    if (startRow + 1 && endRow) {
-      for (let i = endRow - 2; i > startRow; i--) {
-        board[i][endColumn - 1] = board[i - 1][endColumn - 1];
-      }
-    }
-    board[startRow][endColumn - 1] = LEFT;
-    //<-
-    const RIGHT = board[endRow - 1][startColumn - 1];
-    if (startColumn + 1 && endColumn) {
-      for (let i = startColumn - 1; i < endColumn - 1; i++) {
-        board[endRow - 1][i] = board[endRow - 1][i + 1];
-      }
-    }
-    board[endRow - 1][endColumn - 1] = DOWN;
-    //^
-    if (startRow + 1 && endRow) {
-      for (let i = startRow - 1; i < endRow - 1; i++) {
-        board[i][startColumn - 1] = board[i + 1][startColumn - 1];
-      }
-    }
-    board[endRow - 2][startColumn - 1] = RIGHT;
-    console.log(board);
-    return board;
+
+  for (let i = startColumn; i < endColumn; i++) {
+    board[endRow][i] = board[endRow][i + 1];
+    min = Math.min(min, board[endRow][i]);
   }
+
+  for (let i = endRow; i > startRow; i--) {
+    board[i][endColumn] = board[i - 1][endColumn];
+    min = Math.min(min, board[i][endColumn]);
+  }
+
+  for (let i = endColumn; i > startColumn + 1; i--) {
+    board[startRow][i] = board[startRow][i - 1];
+    min = Math.min(min, board[startRow][i]);
+  }
+  board[startRow][startColumn + 1] = tmp;
+
+  return min;
+}
