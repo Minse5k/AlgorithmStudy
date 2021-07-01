7650868ad9df52a951e6af145f5ced39d07d2401
Sunjae95 <tjswo9502@gmail.com>
2021-07-02 00:20:56 +0900

    선재_셔틀버스

--- programmers/LEVEL3/셔틀버스/seonjae_day37.js
+++ programmers/LEVEL3/셔틀버스/seonjae_day37.js
@@ -57,3 +57,79 @@ function solution(n, t, m, timetable) {
 
   return 0;
 }
+
+//2번째 풀이
+function getTime(time) {
+  const tmpH = Math.floor(time / 60);
+  const tmpM = time % 60;
+  const hour = tmpH > 9 ? tmpH.toString() : "0" + tmpH;
+  const minute = tmpM > 9 ? tmpM.toString() : "0" + tmpM;
+  return hour + ":" + minute;
+}
+
+function solution(n, t, m, timetable) {
+  const crew = timetable
+    .map((value) => {
+      const time = value.split(":");
+      return parseInt(time[0]) * 60 + parseInt(time[1]);
+    })
+    .sort((a, b) => b - a);
+  let time = 540;
+  const bus = [];
+  for (let i = 0; i < n; i++) {
+    if (crew.length === 0) return getTime(540 + (n - 1) * t);
+    time = time + t * i;
+    const tmp = [];
+    for (let j = 0; j < m; j++) {
+      const lastIndex = crew.length - 1;
+      if (lastIndex < 0) break;
+      if (time < crew[lastIndex]) break;
+      tmp.push(crew.pop());
+    }
+    bus.push(tmp);
+  }
+
+  if (bus[n - 1].length === m) {
+    const lastIndex = bus[n - 1].length - 1;
+    return getTime(bus[n - 1][lastIndex] - 1);
+  }
+
+  return getTime(time);
+}
+
+//3번째 풀이
+function getTime(time) {
+  const tmpH = Math.floor(time / 60);
+  const tmpM = time % 60;
+  const hour = tmpH > 9 ? tmpH.toString() : "0" + tmpH;
+  const minute = tmpM > 9 ? tmpM.toString() : "0" + tmpM;
+  return hour + ":" + minute;
+}
+
+function solution(n, t, m, timetable) {
+  const crew = timetable
+    .map((value) => {
+      const time = value.split(":");
+      return parseInt(time[0]) * 60 + parseInt(time[1]);
+    })
+    .sort((a, b) => b - a);
+  let time;
+  const bus = [];
+  for (let i = 0; i < n; i++) {
+    time = 540 + t * i;
+    const tmp = [];
+    for (let j = 0; j < m; j++) {
+      const lastIndex = crew.length - 1;
+      if (lastIndex < 0) return getTime(540 + (n - 1) * t);
+      if (time < crew[lastIndex]) break;
+      tmp.push(crew.pop());
+    }
+    bus.push(tmp);
+  }
+
+  if (bus[n - 1].length === m) {
+    const lastIndex = bus[n - 1].length - 1;
+    return getTime(bus[n - 1][lastIndex] - 1);
+  }
+  return getTime(time);
+}
